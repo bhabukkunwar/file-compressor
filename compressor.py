389606bac208b6b3b371b01ba8e4e3f7d241c71b
bhabukkunwar <bhabukunwar@gmail.com>
2024-12-10 17:03:47 +0545

    implemented three algos gzip, lzma and bz2

--- compressor.py
+++ compressor.py
@@ -1,5 +1,14 @@
 import gzip
+import lzma
+import bz2
+import shutil
 
-content = b"Lots of content here"
-with gzip.open('/home/file.txt.gz', 'wb') as f:
-    f.write(content)
+with open('./largefile.txt', 'rb') as f_in, gzip.open('/home/compressed-files/largefile.txt.gz', 'wb') as f_out:
+    shutil.copyfileobj(f_in, f_out)
+with open('./largefile.txt', 'rb') as f_in, bz2.open('/home/compressed-files/largefile.txt.bz2', 'wb') as f_out:
+    shutil.copyfileobj(f_in, f_out)
+with open('./largefile.txt', 'rb') as f_in, lzma.open('/home/compressed-files/largefile.txt.lzma', 'wb') as f_out:
+    shutil.copyfileobj(f_in, f_out)
+
+with gzip.open('/home/compressed-files/largefile.txt.gz', 'rb') as f_in, open('/home/decompressed-files/largefile.txt', 'wb') as f_out:
+    shutil.copyfileobj(f_in,f_out)
